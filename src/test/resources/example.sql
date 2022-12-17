9945c2544cd512ad2023782c7c10d5e89d2b4f4f
NikitaBez <bezmenov.nikita@gmail.com>
2022-12-17 12:56:08 +0900

    Plays with tables, pull the code from netology repo

--- src/test/resources/example.sql
+++ src/test/resources/example.sql
@@ -12,3 +12,35 @@ CREATE TABLE cards
     balance_in_kopecks  INT                NOT NULL DEFAULT 0, /*не может быть null, и если поле не заполнено, то дефолтное значение поля 0 (ноль)*/
     FOREIGN KEY (user_id) REFERENCES users (id) /*foreign key - внешний ключ - поле user_id связано с полем id в таблице users - созданные в этом проекте таблицы связаны между собой этими параметрами*/
 );
+INSERT INTO users (id, login, password) VALUES (1, 'vasya', "password");/* - вставь в таблицу users с полями (указанные в скобках) значения (указанные в скобках)*/
+INSERT INTO users (id, login, password) VALUES (2,'petya', "password");
+
+INSERT INTO cards (id, user_id, number, balance_in_kopecks)
+VALUES (1, 1, '5559000000000001', 1000000),
+       (2, 1, '5559000000000002', 1000000);
+
+UPDATE cards SET balance_in_kopecks = balance_in_kopecks - 10000 /*- обновить в таблице cards установить значение */
+WHERE number = '5559000000000001';                                /*где номер карты ранен .....*/
+UPDATE cards SET balance_in_kopecks = balance_in_kopecks + 10000
+WHERE number = '5559000000000002';
+
+DELETE FROM users WHERE login = 'vasya'; /*-удалить из таблицы users где в логине присутствует vasya - но сначала надо удалить значения во второстепенной таблице, значения в которой привязаны к таблице, из которой хотим удалить значения*/
+
+DELETE   /*удалить*/
+FROM cards   /*из таблицы cards*/
+WHERE user_id IN (SELECT id FROM users WHERE login = 'vasya'); /*где user_id связанная с id из таблицы users, связанные с логином 'vasya */
+
+ -- выборка всех столбцов и всех строк из таблицы users (будьте острожны на больших таблицах)
+SELECT * FROM users;
+
+ -- выборка только определенных столбцов
+SELECT id, login FROM users;
+
+ -- выборка по условию
+ SELECT balance_in_kopecks FROM cards WHERE number = '5559000000000002';
+
+ -- вычисляемые столбцы
+SELECT max(cards.balance_in_kopecks) FROM cards;
+SELECT max(balance_in_kopecks) FROM cards WHERE user_id=1;
+
+SELECT count(*), user_id FROM cards GROUP BY user_id;
--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,8 +1,1 @@
-CREATE TABLE cards
-(
-    id                  INT PRIMARY KEY AUTO_INCREMENT,
-    user_id             INT                NOT NULL,
-    number              VARCHAR(19) UNIQUE NOT NULL,
-    balance_in_kopecks  INT                NOT NULL DEFAULT 0,
-    FOREIGN KEY (user_id) REFERENCES users (id)
-);
+SELECT count(*), user_id FROM cards GROUP BY user_id;
